0a88d615f8837b6551b264b13f7a3a287d9efee7
goldenguy00 <goldenguy00@gmail.com>
2025-05-11 23:10:05 -0400

    elite tiers enum

---
+++ R2API.Elites/EliteTier.cs
@@ -0,0 +1,48 @@
+﻿namespace R2API;
+
+/// <summary>
+/// The vanilla elite tiers.
+/// Can be casted to an int to index into the EliteAPI.VanillaEliteTiers array.
+/// </summary>
+public enum EliteTier
+{
+    /// <summary>
+    /// No Tier. Reserved for spawning non-elites.
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Tier 1.
+    /// Spawns on stages 1 and 2.
+    /// </summary>
+    Tier1,
+
+    /// <summary>
+    /// Tier 1 Honor.
+    /// Spawns on stages 1 and 2 with the Honor artifact enabled.
+    /// </summary>
+    Tier1Honor,
+
+    /// <summary>
+    /// Guilded Tier 1 Honor.
+    /// Spawns on stage 3 and above with the Honor artifact enabled.
+    /// </summary>
+    GuildedTier1Honor,
+
+    /// <summary>
+    /// Guilded Tier 1.
+    /// Spawns on stage 3 and above.
+    /// </summary>
+    GuildedTier1,
+
+    /// <summary>
+    /// Tier 2.
+    /// Spawns after the first loop.
+    /// </summary>
+    Tier2,
+
+    /// <summary>
+    /// Lunar tier. Spawns on the moon only.
+    /// </summary>
+    Lunar
+}
